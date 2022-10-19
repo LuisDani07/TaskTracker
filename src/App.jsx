736dc25a6b8cb50499a69baae22f9ddad6d04c4f
LuisDani07 <tics270118@gmail.com>
2022-10-18 22:14:18 -0600

    diseño aplicado con tailWind css

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,14 @@ import TaskForm from './components/TaskForm'
 import {useEffect, useState} from 'react'
 function App() {
 
-
-
-
-
-
-
   return (
-    <>
-            <TaskForm />
-             <TaskList />
-    </>
+    <main className='bg-zinc-900 h-screen '>
+            <div className='container mx-auto p-10'>
+               <TaskForm />
+                <TaskList />
+            </div>
+
+    </main>
 
   )
 }
--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -22,11 +22,15 @@ const handleSubmit=(e)=>{
 
 
   return (
-        <form onSubmit={handleSubmit}>
-              <input placeholder="Escribe tu tarea" onChange={function(e){setTitle(e.target.value)}} value={title} autoFocus/>
-              <textarea placeholder='escribe la descripción de la tarea' onChange={(e)=>{setDescription(e.target.value)}} value={description}></textarea>
-               <button >Guardar</button>
-        </form>
+    <div className='max-w-md mx-auto'>
+                        <form onSubmit={handleSubmit} className="bg-slate-800 p-10 mb-4">
+                          <h1 className='text-2xl font-bold text-white mb-3 text-center'>crea tu tarea</h1>
+              <input className='bg-slate-300 p-3 w-full mb-2' placeholder="Escribe tu tarea" onChange={function(e){setTitle(e.target.value)}} value={title} autoFocus/>
+              <textarea  className='bg-slate-300 p-3 w-full mb-2' placeholder='escribe la descripción de la tarea' onChange={(e)=>{setDescription(e.target.value)}} value={description}></textarea>
+               <button className='bg-indigo-500 px-3 py-1 text-white'>Guardar</button>
+                </form>
+    </div>
+
   )
 }
 
